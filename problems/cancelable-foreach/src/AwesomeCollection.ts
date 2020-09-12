d8bd09658d7ececc4b39f2ce63494cf662ce8577
jamashita <jamashita@arachnid.xyz>
2020-09-13 06:46:34 +0900

    awesome collection

--- problems/cancelable-foreach/src/AwesomeCollection.ts
+++ problems/cancelable-foreach/src/AwesomeCollection.ts
@@ -1,6 +1,11 @@
 import { Callback, CancelableForEach } from './CancelableForEach';
 
 export class AwesomeCollection implements CancelableForEach<unknown> {
+  public constructor(...values: Array<unknown>) {
+    // TODO
+    console.log(values);
+  }
+
   public forEach(cb: Callback<unknown>): void {
     // TODO
     console.log(cb);
