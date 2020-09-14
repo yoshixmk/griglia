1c375aaacfe77c001faef2deec0d803ab1d63882
jamashita <jamashita@arachnid.xyz>
2020-09-15 00:14:38 +0900

    pagination

---
+++ problems/pagination/jest.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  verbose: true,
+  bail: true,
+  testRegex: '/__tests__/.+\\.spec\\.tsx?$',
+  testPathIgnorePatterns: [
+    'node_modules',
+    'dist'
+  ],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    'node_modules'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx'
+  ],
+  testURL: 'http://localhost'
+};
