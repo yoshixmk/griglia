5e958e2e154546187dd9a458db276b9b958269e7
jamashita <jamashita@arachnid.xyz>
2020-09-20 16:05:18 +0900

    feat: 🎸 compex number

---
+++ problems/number-range/src/Numbers/ComplexNumber.ts
@@ -0,0 +1,68 @@
+import { RangeElement } from '../RangeElement';
+
+export class ComplexNumber implements RangeElement {
+  private readonly ranges: Array<RangeElement>;
+
+  public static of(ranges: Array<RangeElement>): ComplexNumber {
+    return new ComplexNumber(ranges);
+  }
+
+  protected constructor(ranges: Array<RangeElement>) {
+    this.ranges = ranges;
+  }
+
+  public contains(num: number): boolean {
+    return this.ranges.some((range: RangeElement) => {
+      return range.contains(num);
+    });
+  }
+
+  public ready(num: number): boolean {
+    return this.ranges.some((range: RangeElement) => {
+      return range.ready(num);
+    });
+  }
+
+  public add(num: number): RangeElement {
+    const ranges: Array<RangeElement> = this.ranges.map<RangeElement>((range: RangeElement) => {
+      if (!range.contains(num)) {
+        return range.add(num);
+      }
+
+      return range;
+    });
+
+    return ComplexNumber.of(ranges);
+  }
+
+  public remove(num: number): RangeElement {
+    const ranges: Array<RangeElement> = this.ranges.map<RangeElement>((range: RangeElement) => {
+      if (range.contains(num)) {
+        return range.remove(num);
+      }
+
+      return range;
+    });
+
+    return ComplexNumber.of(ranges);
+  }
+
+  public serialize(): string {
+    return this.ranges.map<string>((range: RangeElement) => {
+      return range.serialize();
+    }).join(', ');
+  }
+
+  public equals(other: RangeElement): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (other instanceof ComplexNumber) {
+      return this.ranges.every((r: RangeElement, i: number) => {
+        return r.equals(other.ranges[i]);
+      });
+    }
+
+    return false;
+  }
+}
